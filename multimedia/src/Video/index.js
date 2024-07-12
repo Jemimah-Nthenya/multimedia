f4a5863fe4a6a49154db45b3c3ae914616ab622f
Jemimah-Nthenya <jemimahogonda@gmail.com>
2024-07-12 07:34:42 +0300

    added changes

---
+++ multimedia/src/Video/index.js
@@ -0,0 +1,25 @@
+import './index.css';
+import video from '/'
+
+const Video = () => {
+    return (
+        <>
+       <div className='all'> 
+       <img src="/images/video.jpg" alt="video" id='image'/>  
+       <div className='pagetext'> <h1  className='text'>Embark on a multimedia journey with our newest video</h1>
+       <div id='btn' > <button >Listen Now</button></div> 
+  
+       </div>
+       
+       </div>
+     
+       </>
+        
+    );
+}
+
+export default  Video;
+
+
+
+
